4107f6a4645ac38a5b219e18e286b063c90d9966
shoyebmahmood <mahmoodshoyeb@gmail.com>
2019-02-10 17:58:34 +0600

    define parameter and arguments

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
-function evenOrOdd($n){
+function evenOrOdd($n){//$n is parameter
     if($n%2==0){
         return true;
     }return false;
 }
 
-$x=11;
-if (evenOrOdd($x)){
+$x=101;
+if (evenOrOdd($x)){//$x is argument
     echo "{$x} is Even number";
 }else{
     echo "{$x} is Odd number";
