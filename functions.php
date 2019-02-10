0aa74927579c26928636c0f06bb978e347ca50e5
shoyebmahmood <mahmoodshoyeb@gmail.com>
2019-02-10 18:01:08 +0600

    Function's name should be easy to understand

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
-function evenOrOdd($n){//$n is parameter
+function isEven($n){//$n is parameter
     if($n%2==0){
         return true;
     }return false;
 }
 
 $x=101;
-if (evenOrOdd($x)){//$x is argument
+if (isEven($x)){//$x is argument
     echo "{$x} is Even number";
 }else{
     echo "{$x} is Odd number";
